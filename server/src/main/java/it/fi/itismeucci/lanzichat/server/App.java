7be9d42ec1459135aaee272d3506b46b1e431d1b
F3kFak <78552165+F3kFak@users.noreply.github.com>
2022-10-12 12:31:22 +0200

    Server Rifatto con Thread

--- server/src/main/java/it/fi/itismeucci/lanzichat/server/App.java
+++ server/src/main/java/it/fi/itismeucci/lanzichat/server/App.java
@@ -4,12 +4,10 @@ package it.fi.itismeucci.lanzichat.server;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
+public class App {
+    public static void main(String[] args) throws Exception
     {
         ServerStr servente = new ServerStr();
-        servente.attendi();
-        servente.comunica();
+        servente.avvia();
     }
 }
--- server/src/main/java/it/fi/itismeucci/lanzichat/server/ServerStr.java
+++ server/src/main/java/it/fi/itismeucci/lanzichat/server/ServerStr.java
@@ -4,38 +4,36 @@ import java.io.*;
 import java.net.*;
 
 public class ServerStr {
-    ServerSocket server = null;
-    Socket client = null;
-    String StringaRicevuta = null;
-    String StringaModificata = null;
-    BufferedReader inDalClient;
-    DataOutputStream outVersoClient;
 
-    public Socket attendi() {
-        try {
-            System.out.println("1 Server partito in esecuzione ... ");
-            server = new ServerSocket(12121);
-            client = server.accept();
-            server.close();
-            inDalClient = new BufferedReader(new InputStreamReader(client.getInputStream()));
-            outVersoClient = new DataOutputStream(client.getOutputStream());
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-            System.out.println("Errore durante l'instanza del server");
-            System.exit(1);
+    public void avvia() throws IOException{
+        System.out.println("1 Server partito in esecuzione ... ");
+        ServerSocket server = new ServerSocket(12121);
+        for(;;){  
+            Socket client;    
+            try {
+                client = server.accept();
+                Thread t = new Thread(() -> comunica(client));
+                t.start();
+            }
+            catch (Exception e) {
+                System.out.println(e.getMessage());
+                System.out.println("Errore durante l'instanza del server");
+                System.exit(1);
+            }
         }
-        return client;
     }
 
-    public void comunica() {
+    public void comunica(Socket client) {
         try {
+            BufferedReader inDalClient = new BufferedReader(new InputStreamReader(client.getInputStream()));
+            DataOutput outVersoClient = new DataOutputStream(client.getOutputStream());
             System.out.println("3 benvenuto client, scrivi una frase e la trasformo in maiscuolo. Attendo ...");
-            StringaRicevuta = inDalClient.readLine();
+            String StringaRicevuta = inDalClient.readLine();
             System.out.println("6 ricevuta la stringa dal cliente: " + StringaRicevuta);
 
-            StringaModificata = StringaRicevuta.toUpperCase();
+            String StringaModificata = StringaRicevuta.toUpperCase();
             System.out.println("7 invio la stringa modificata al client ...");
-            outVersoClient.writeBytes(StringaModificata+'\n');
+            outVersoClient.writeBytes(StringaModificata + '\n');
 
             System.out.println("9 SERVER: fine elaborazione ... frat'm ingiustamente incarcerato");
             client.close();
@@ -44,5 +42,6 @@ public class ServerStr {
             System.out.println("Errore durante l'instanza del server");
             System.exit(1);
         }
+        // server.close();
     }
 }
